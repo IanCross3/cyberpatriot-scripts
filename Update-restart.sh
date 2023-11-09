f19e75b4cdb1ee985bd89f7356a29a8c738ecf9e
IanCross3 <122713281+IanCross3@users.noreply.github.com>
2023-11-09 18:48:43 -0500

    Update Update-restart.sh
    added an option-menu style loop to ask what script to execute

--- Update-restart.sh
+++ Update-restart.sh
@@ -1,9 +1,55 @@
 #!/bin/bash
+update_and_restart(){
 
-echo "Updating and Installing"
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-echo "Finished updating"
-sleep 10
-sudo reboot
+    echo "Updating and Installing"
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    sudo apt-get -y dist-upgrade
+    echo "finished updating"
+    sleep 10
+    sudo reboot
+}
+
+placeholder() {
+
+    echo "placeholder"
+}
+
+
+placeholder1() {
+    echo "placeholder"
+}
+
+main_menu() {
+    clear
+    echo "Select an option:"
+    echo "1. Update and restart"
+    echo "2. Another Task"
+    echo "3. Different Task"
+    echo "4. Exit"
+
+    read -p "Enter the number corresponding to your choice: " choice
+
+    case $choice in
+        1)
+            update_and_restart
+            ;;
+        2)
+            placeholder
+            ;;
+        3)
+            placeholder1
+            ;;
+        4)
+            echo "Exiting program."
+            exit
+            ;;
+        *)
+            echo "Invalid choice. Please enter a number from the menu."
+            ;;
+    esac
+}
+
+while true; do
+    main_menu
+done
